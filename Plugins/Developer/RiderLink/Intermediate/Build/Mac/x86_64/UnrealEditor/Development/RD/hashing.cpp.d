39e3e43a9d644f26535ed5b83e988d979abc03eb
Above Noise Studios <113855715+abovenoisestudios@users.noreply.github.com>
2022-10-28 23:40:37 -0500

    Deleted Plugins Directory

--- Plugins/Developer/RiderLink/Intermediate/Build/Mac/x86_64/UnrealEditor/Development/RD/hashing.cpp.d
+++
@@ -1,3 +0,0 @@
-/Volumes/Projects/6.\ REPOSITORIES/2022/10.\ Oct/ANE_Blog_UE5_MetasoundQuartz_InteractiveMusic1/Plugins/Developer/RiderLink/Intermediate/Build/Mac/x86_64/UnrealEditor/Development/RD/hashing.cpp.o: \
-  /Volumes/Projects/6.\ REPOSITORIES/2022/10.\ Oct/ANE_Blog_UE5_MetasoundQuartz_InteractiveMusic1/Plugins/Developer/RiderLink/Source/RD/src/rd_framework_cpp/src/main/util/hashing.cpp \
-  /Volumes/Projects/6.\ REPOSITORIES/2022/10.\ Oct/ANE_Blog_UE5_MetasoundQuartz_InteractiveMusic1/Plugins/Developer/RiderLink/Intermediate/Build/Mac/x86_64/UnrealEditor/Development/RD/Definitions.h
